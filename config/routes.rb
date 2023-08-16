cb64eaa8a8e722f102449823c547565fe0c3ebe5
Yogesh-Kumar-Chittodiya <yk843991@gmail.com>
2023-08-16 12:43:13 +0530

    Initial Commit

---
+++ config/routes.rb
@@ -0,0 +1,20 @@
+Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
+
+  get '/member_details' => 'members#index'
+
+  # resources :companies
+
+  namespace :api do
+    namespace :v1 do
+      resources :companies
+    end
+  end
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
